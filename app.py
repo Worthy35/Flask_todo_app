c379b58353601648f64c3e6c89837fdacf334f7b
Shayan <168076200+Worthy35@users.noreply.github.com>
2025-10-22 20:54:45 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # جلوگیری از کش شدن فایل‌های static
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 tasks = []
 
